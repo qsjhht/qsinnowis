576f59608e1341a6de5aca74c3d923a46b0280ca
ke_lin10 <ke_lin10@126.com>
2019-08-24 18:36:29 +0800

    public

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+namespace think;
+
+if (version_compare(PHP_VERSION, '5.6.0', '<')) {
+    header("Content-type: text/html; charset=utf-8");
+    die('PHP 5.6.0 及以上版本系统才可运行~ ');
+}
+
+define('IF_PUBLIC', true);
+define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+define('STATIC_PATH',ROOT_PATH. 'static' .DIRECTORY_SEPARATOR);
+define('APP_PATH', ROOT_PATH . '../application' . DIRECTORY_SEPARATOR);
+define('ROOT_URL', rtrim(dirname($_SERVER["SCRIPT_NAME"]), '\\/') . '/');
+
+// 加载基础文件
+require __DIR__ . '/../thinkphp/base.php';
+
+// 支持事先使用静态方法设置Request对象和Config对象
+
+// 执行应用并响应
+Container::get('app')->run()->send();
