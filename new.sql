668439794201e8f4f8e4f8cfa96403d6d737bd9e
kelinh <ke_lin10@126.com>
2019-08-27 01:18:56 +0800

    sql

--- new.sql
+++ new.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2019-08-26 18:50:59
+Date: 2019-08-27 01:18:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -184,7 +184,6 @@ INSERT INTO `qs_dept` VALUES ('105', '0', '设备部', '大厅一层 115', 'adsf
 INSERT INTO `qs_dept` VALUES ('106', '0', '档案部', '大厅二层 210', 'afddddddd', '管理档案的', '', '0', '106');
 INSERT INTO `qs_dept` VALUES ('107', '0', '入廊企业部', '大厅二层 201', 'aaaaaaaaaaaaa', '入廊企业管理的', '', '0', '107');
 INSERT INTO `qs_dept` VALUES ('108', '0', '人员管理部', '大厅一层 102', 'tsghfnherttr', '人员管理的', '', '0', '108');
-INSERT INTO `qs_dept` VALUES ('109', '101', '发噶', '大厅一层 102', 'ads', 'afssdaf', null, '1', '109');
 INSERT INTO `qs_dept` VALUES ('118', '104', '修水管', '啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊', null, '啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊', '啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊', '0', '118');
 
 -- ----------------------------
@@ -328,16 +327,14 @@ CREATE TABLE `qs_role` (
   `role_auth_names` varchar(255) DEFAULT NULL,
   `role_detail` tinytext,
   PRIMARY KEY (`role_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of qs_role
 -- ----------------------------
-INSERT INTO `qs_role` VALUES ('1', '职工', '#-#,people-index,#-#,enterprise-index,patrol-index', '108,106,107,105,104', '3D漫游,人员管理,应急指挥,入廊企业,巡查巡检', '职工');
-INSERT INTO `qs_role` VALUES ('3', '主管', 'patrol-index,enterprise-index,people-index,#-#', '104,105,106,107', '巡查巡检,入廊企业,人员管理,应急指挥', '主管');
-INSERT INTO `qs_role` VALUES ('4', '精英', 'run-index,eqpt-index,warning-index', '101,102,103', '运行管理,设备管理,报警信息', 'asdf');
-INSERT INTO `qs_role` VALUES ('5', '荟萃', 'run-index,patrol-index,enterprise-index,people-index', '101,104,105,106', '运行管理,巡查巡检,入廊企业,人员管理', 'asdfasdfasd');
-INSERT INTO `qs_role` VALUES ('6', '阿斯蒂芬', 'run-index,eqpt-index,warning-index', '101,102,103', '运行管理,设备管理,报警信息', 'sdf阿萨德发射点发');
+INSERT INTO `qs_role` VALUES ('8', 'aaaa', 'Visual-index,Visual-bim,Enterprise-index,Eqpt-ledger,Patrol-live,Patrol-setting,Patrol-manage,Patrol-log', '101,102,110,117,118,119,120,106,', '大数据,bim,设备台账,巡查巡检实况,路线规划,任务管理,任务日志,教育培训,', 'vvv');
+INSERT INTO `qs_role` VALUES ('10', '圣达菲', 'Eqpt-ledger,Eqpt-cancel,Eqpt-disassemble,Service-index,Replace-index,Eqpt-plan,Eqpt-forms,Video-liveplay,Video-replay', '110,111,112,113,114,115,116,124,125,', '设备台账,销账处理,设备拆解,设备维修,设备更换,维护计划,维护报表,实时视频,视频回放,', '打算范德萨发');
+INSERT INTO `qs_role` VALUES ('9', 'asd', 'Eqpt-ledger,Eqpt-cancel,Eqpt-forms', '110,111,116,', '设备台账,销账处理,维护报表,', 'asdasd');
 
 -- ----------------------------
 -- Table structure for qs_services
