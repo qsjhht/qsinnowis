59b317ca9ef1278b7684db824af8867047224526
ke_lin10 <ke_lin10@126.com>
2019-10-15 10:10:22 +0800

    Real time api

--- application/api/controller/Real.php
+++ application/api/controller/Real.php
@@ -45,4 +45,55 @@ class Real  extends Controller
         $return_data['data'] = $real;*/
         echo json_encode($realdata,JSON_UNESCAPED_UNICODE);die;
     }
+    public function real_times()
+    {
+        $zone = $this->request->param('zone');
+        $name = $this->request->param('name');
+        //$last_time = Db::connect('yk_config')->table('newtable')->field('TIME')->order('TIME','desc')->limit(1)->find();
+        /*$real =Db::connect('yk_config')
+            ->table('history')
+            ->where('TIME','=',function($query){
+                $query->table('history')->field('TIME')->order('TIME','desc')->limit(1);
+            })
+            ->select();*/
+        $realdata = [];
+        /*$real = Db::connect('yk_config')->table('newtable')
+            ->where('TIME','in',function($query){
+                $query->table('newtable')->field('TIME')->order('TIME','desc')->limit(1000);
+            })
+            ->field('TIME,VALUE')
+            ->where('ZONE',$zone)
+           // ->where('NAME',$name)
+            ->select();*/
+        $real_times = Db::connect('yk_config')
+            ->table('newtable')
+            ->field('TIME,VALUE')
+            ->order('TIME','desc')
+            ->where('NAME',$name)
+            ->limit(100)
+            ->select();
+        //dump($last_time);
+        /*$return_data['code'] = 200;
+        $return_data['msg']  = '查询成功！';
+        $return_data['data'] = $real;*/
+        foreach ($real_times as $value){
+            $realdata[$value['TIME']] = $value['VALUE'];
+        }
+        echo json_encode($realdata,JSON_UNESCAPED_UNICODE);die;
+    }
+
+    public function get_last_real()
+    {
+        $zone = $this->request->param('zone');
+        $name = $this->request->param('name');
+        $real_times = Db::connect('yk_config')
+            ->table('newtable')
+            ->field('TIME,VALUE')
+            ->order('TIME','desc')
+            ->where('NAME',$name)
+            ->find();
+        $realdata = [];
+        $realdata[$real_times['TIME']] = $real_times['VALUE'];
+        echo json_encode($realdata,JSON_UNESCAPED_UNICODE);die;
+    }
 }
