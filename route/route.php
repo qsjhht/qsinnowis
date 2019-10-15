c6ef6e1d16ad7f84ab00831fe489d0cf6850b0c9
ke_lin10 <ke_lin10@126.com>
2019-10-15 10:18:44 +0800

    route config add realTime api route

--- route/route.php
+++ route/route.php
@@ -13,3 +13,17 @@
 Route::post('patrol/data', 'api/patrol/data')->allowCrossDomain();
 
 Route::get('patrol/eqpt_data', 'api/patrol/eqpt_data')->allowCrossDomain();
+
+Route::get('patrol/get_type', 'api/patrol/get_type')->allowCrossDomain();
+
+Route::get('patrol/get_patrol', 'api/patrol/get_patrol')->allowCrossDomain();
+
+Route::get('patrol/get_patrol_json', 'api/patrol/get_patrol_json')->allowCrossDomain();
+
+Route::get('patrol/get_user_position', 'api/patrol/get_user_position')->allowCrossDomain();
+
+
+
+Route::get('real/real_time', 'api/real/real_time')->allowCrossDomain();
+Route::get('real/real_times', 'api/real/real_times')->allowCrossDomain();
+Route::get('real/get_last_real', 'api/real/get_last_real')->allowCrossDomain();
