d42f950c690a971c9cbb47de2ce599942cd7729f
kelinh <ke_lin10@126.com>
2019-10-29 07:45:24 +0800

    enterprise

--- application/index/controller/Enterprise.php
+++ application/index/controller/Enterprise.php
@@ -140,7 +140,25 @@ class Enterprise extends Adminbase
     public function info_edit()
     {
         if ($this->request->isPost()) {
-
+            $data = $this->request->post();
+            $data['en_time'] = strtotime($data['en_time']);
+            $data['en_start'] = '';
+            $data['en_end'] = '';
+            foreach ($data['start'] as $v1) {
+                $data['en_start'] .= $v1.'-';
+            }
+            foreach ($data['end'] as $v2) {
+                $data['en_end'] .= $v2.'-';
+            }
+            $data['co_local'] = $data['province'] .'-'. $data['city']  .'-'. $data['county']  .'-'. $data['add'];
+//            dump($data);
+            $res = Db('enterprise')
+                ->where('co_id',$data['co_id'])
+                ->strict(false)
+                ->update($data);
+            if ($res){
+                $this->success('编辑企业信息成功！');
+            }$this->error('编辑企业信息失败！');
         }else{
             $co_id = $this->request->param('co_id');
             $field = "e.co_id,e.co_name,e.co_linker,e.co_local,e.co_type,e.co_linker_post,e.en_start,e.en_end,e.co_legal,e.co_phone,e.en_remark,e.en_long,e.en_time,t.type_name,u.user_name,u.user_id";
@@ -166,7 +184,14 @@ class Enterprise extends Adminbase
 
             $sites_data = json_encode($this->Patrol->getSites());
             $this->assign('Sitesdata', $sites_data);
+            $local = explode('-',$res['co_local']);
+            $this->assign('Local', json_encode($local,JSON_UNESCAPED_UNICODE));
+            $this->assign('Startstr', json_encode($startarr,JSON_UNESCAPED_UNICODE));
+            $this->assign('Endstr', json_encode($endarr,JSON_UNESCAPED_UNICODE));
+            $this->assign('Time', $res['en_time']);
+            $this->assign('Add', $local['3']);
 
+            $this->assign('Sitesdata', $sites_data);
             $co_type = Db('co_type')->select();
             $this->assign('Co_type',$co_type);
             $user = Db('user')->field('user_id,user_name')->select();
@@ -176,11 +201,59 @@ class Enterprise extends Adminbase
         }
     }
 
+//    删除企业信息
+    public function info_del()
+    {
+        $co_id = $this->request->param('co_id');
+        if (empty($co_id)) {
+            $this->error('ID错误');
+        }
+        if(Db('enterprise')->delete($co_id)){
+            $this->success('删除企业信息成功！');
+        }$this->error('删除企业信息失败！');
+    }
+
     public function construction()
     {
         return $this->fetch();
     }
 
+    public function work_add()
+    {
+        if ($this->request->isPost()) {
+
+            $data = $this->request->post();
+            $data['en_time'] = strtotime($data['en_time']);
+            $data['en_start'] = '';
+            $data['en_end'] = '';
+            foreach ($data['start'] as $v1) {
+                $data['en_start'] .= $v1.'-';
+            }
+            foreach ($data['end'] as $v2) {
+                $data['en_end'] .= $v2.'-';
+            }
+            $data['co_local'] = $data['province'] .'-'. $data['city']  .'-'. $data['county']  .'-'. $data['add'];
+            $res = Db('enterprise')
+                ->strict(false)
+                ->insert($data);
+            if ($res){
+                $this->success('新增企业信息成功！');
+            }$this->error('新增企业信息失败！');
+        }else{
+            $local = '[{"id":1,"pid":0,"title":"河北省"},{"id":2,"pid":0,"title":"北京"},{"id":3,"pid":0,"title":"天津"},{"id":4,"pid":1,"title":"石家庄市"},{"id":5,"pid":1,"title":"唐山市"},{"id":6,"pid":1,"title":"秦皇岛市"},{"id":7,"pid":1,"title":"邯郸市"},{"id":8,"pid":1,"title":"邢台市"},{"id":9,"pid":1,"title":"保定市"},{"id":10,"pid":1,"title":"张家口市"},{"id":11,"pid":1,"title":"承德市"},{"id":12,"pid":1,"title":"沧州市"},{"id":13,"pid":1,"title":"廊坊市"},{"id":14,"pid":1,"title":"衡水市"},{"id":15,"pid":4,"title":"长安区"},{"id":16,"pid":4,"title":"桥东区"},{"id":17,"pid":4,"title":"桥西区"},{"id":18,"pid":4,"title":"新华区"},{"id":19,"pid":4,"title":"井陉矿区"},{"id":20,"pid":4,"title":"裕华区"},{"id":21,"pid":4,"title":"井陉县"},{"id":22,"pid":4,"title":"正定县"},{"id":21,"pid":4,"title":"栾城区"},{"id":21,"pid":4,"title":"行唐县"}]';
+            $this->assign('local_data', $local);
+
+            $sites_data = json_encode($this->Patrol->getSites());
+            $this->assign('Sitesdata', $sites_data);
+
+            $co_type = Db('co_type')->select();
+            $this->assign('Co_type',$co_type);
+
+            $user = Db('user')->field('user_id,user_name')->select();
+            $this->assign('User',$user);
+            return $this->fetch();
+        }
+    }
 
 
 }
