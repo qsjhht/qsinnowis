aeb540212e1599c50d3343aa8f4e5e64f547fcb0
ke_lin10 <ke_lin10@126.com>
2020-03-27 15:08:16 +0800

    二级菜单 排序

--- application/index/controller/Alarm.php
+++ application/index/controller/Alarm.php
@@ -26,7 +26,7 @@ class Alarm extends Adminbase
     {
         //根据当前控制器名 ->auth_id  ->子类
         $auth_id = Db('auth')->where('auth_c',$this->request->controller())->where('auth_pid','0')->field('auth_id')->find();
-        $sub = Db('auth')->where('auth_pid',$auth_id['auth_id'])->select();
+        $sub = Db('auth')->where('auth_pid',$auth_id['auth_id'])->order('auth_id','esc')->select();
         $defaultUrl = ($sub['0']['auth_isLink']) ? $sub['0']['auth_url'] : $sub['0']['auth_c'].'/'.$sub['0']['auth_a'];
         $is_http = ($sub['0']['auth_isLink']) ? 1 : 0;
         $this->assign('Sub',$sub);
