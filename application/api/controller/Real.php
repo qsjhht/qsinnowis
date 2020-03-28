a379dd31ce7e635a36a8b450110151b10295b806
ke_lin10 <ke_lin10@126.com>
2020-03-28 12:26:37 +0800

    新增 窗口设置api 俩

--- application/api/controller/Real.php
+++ application/api/controller/Real.php
@@ -7,6 +7,8 @@
  */
 
 namespace app\api\controller;
+use app\index\model\Realsql as RealsqlModel;
+use app\index\model\Realsql;
 use think\Controller;
 use think\Db;
 use think\Request;
@@ -284,4 +286,85 @@ class Real extends Common
 //        $realdata[$real_times['TIME']] = $real_times['VALUE'];
 //        echo json_encode($realdata,JSON_UNESCAPED_UNICODE);die;
     }
+
+    public function status_refresh()
+    {
+        return  'false';
+    }
+
+    public function call_ericsson()
+    {
+
+//        $msg = 'hello';
+//        $handle = stream_socket_client("udp://127.0.0.1:22335", $errno, $errstr);
+//        if( !$handle ){
+//            die("ERROR: {$errno} - {$errstr}\n");
+//        }
+//        fwrite($handle, $msg."\n");
+//        $result = fread($handle, 1024);
+//        fclose($handle);
+
+        //创建一个socket套接流
+        $socket = socket_create(AF_INET,SOCK_DGRAM,SOL_UDP);
+        /****************设置socket连接选项，这两个步骤你可以省略*************/
+        //接收套接流的最大超时时间1秒，后面是微秒单位超时时间，设置为零，表示不管它
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec" => 1, "usec" => 0));
+        //发送套接流的最大超时时间为6秒
+        socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array("sec" => 1, "usec" => 0));
+        /****************设置socket连接选项，这两个步骤你可以省略*************/
+
+        //连接服务端的套接流，这一步就是使客户端与服务器端的套接流建立联系
+        if(socket_connect($socket,'127.0.0.1',22334) == false){
+            echo 'connect fail massege:'.socket_strerror(socket_last_error());
+        }else{
+            $message = 'call somebody Ericsson Ericsson Ericsson';
+            //转为GBK编码，处理乱码问题，这要看你的编码情况而定，每个人的编码都不同
+            $message = mb_convert_encoding($message,'GBK','UTF-8');
+            //向服务端写入字符串信息
+
+            if(socket_write($socket,$message,strlen($message)) == false){
+                echo 'fail to write'.socket_strerror(socket_last_error());
+
+            }else{
+                echo 'client write success'.PHP_EOL;
+                //读取服务端返回来的套接流信息
+//                while($callback = socket_read($socket,1024)){
+//                    echo 'server return message is:'.PHP_EOL.$callback;
+//                }
+            }
+        }
+        socket_close($socket);//工作完毕，关闭套接流
+
+
+    }
+
+    public function get_video_set()
+    {
+        $sets = db('video_set')
+            ->select();
+        echo json_encode($sets,JSON_UNESCAPED_UNICODE);die;
+    }
+
+    public function video_set()
+    {
+        $realsql = new RealsqlModel();
+        $sets = $this->request->param();
+        $arr = [];
+        $arrs = [];
+        foreach ($sets as $key=>$val) {
+            $key = substr($key,2);
+            $arr['id'] = $key;
+            $arr['token'] = $val;
+            $arrs[] = $arr;
+        }
+        $res = $realsql->saveAll($arrs);
+        if($res){
+            $this->return_msg(200, '更新成功！');
+        }
+//
+    }
+
+
+
+
 }
