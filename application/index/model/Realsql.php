6005c2acc847400a50866b59b3332c7b2027ae63
ke_lin10 <ke_lin10@126.com>
2020-03-28 12:32:57 +0800

    新增 窗口设置api model

---
+++ application/index/model/Realsql.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/26
+ * Time: 18:37
+ */
+
+namespace app\index\model;
+
+use think\Model;
+use think\Db;
+
+class Realsql extends Model
+{
+    protected $table = 'qs_video_set';
+}
