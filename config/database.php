5a5b7d404a9136bb11402455f6094ac5b9ac2b2a
ke_lin10 <ke_lin10@126.com>
2020-04-01 18:05:59 +0800

    亚控 sqlsrv 配置

--- config/database.php
+++ config/database.php
@@ -64,13 +64,13 @@ return [
         // 数据库类型
         'type'            => 'sqlsrv',
         // 服务器地址
-        'hostname'        => '192.168.10.21',
+        'hostname'        => '192.168.5.112',
         // 数据库名
         'database'        => 'ZD_PIPE',
         // 用户名
-        'username'        => 'qs_users', //qs_user
+        'username'        => 'sa', //qs_user
         // 密码
-        'password'        => '1qaz@WSXj', //'qs@123',
+        'password'        => 'Admin123', //'qs@123',
         // 端口
         'hostport'        => '1433',
     ],
