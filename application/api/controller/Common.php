1a4baa1315e43db73233c1a91cfda7f3479068ac
ke_lin10 <ke_lin10@126.com>
2020-04-01 17:46:38 +0800

    编码转译 方法

--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -371,4 +371,13 @@ class Common extends Controller
         break;*/
         }
     }
+
+    //判断并转码
+    public function doEncoding($str){
+        $encode = strtoupper(mb_detect_encoding($str, ["ASCII",'UTF-8',"GB2312","GBK",'BIG5']));
+        if($encode!='UTF-8'){
+            $str = mb_convert_encoding($str, 'UTF-8', $encode);
+        }
+        return $str;
+    }
 }
