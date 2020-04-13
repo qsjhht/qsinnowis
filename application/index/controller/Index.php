0824dd39ecf5f0e5885867da23e4e1a58bf48ac5
ke_lin10 <ke_lin10@126.com>
2020-04-13 20:18:08 +0800

    扫描下载app 二维码 qrcode 类库使用

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,15 +4,36 @@ use think\Controller;
 use think\Db;
 use app\common\controller\Adminbase;
 use app\index\model\AdminUser as AdminUser_model;
+require_once STATIC_PATH.'phpqrcode\phpqrcode.php';
 
 class Index extends Adminbase
 {
+    protected $filename;
+    protected $QRcode;
+    protected $url;
+    protected $Reimg;
+    protected function initialize()
+    {
+        parent::initialize();
+        $this->QRcode = new \QRcode();
+        $this->filename = ROOT_PATH.'qrcode/qrcodes.png';
+        $this->logo = ROOT_PATH.'qrcode\logo4.jpg'; //准备好的logo背景图片
+        $this->Reimg = ROOT_PATH.'qrcode\Reimg.jpeg'; //组合好的二维码图片
+        $this->apk = ROOT_PATH.'qrcode\kid-ward.apk';
+        $this->url = 'http://'.$_SERVER['SERVER_NAME'].'/index/index/download';
+    }
     public function index()
     {
+
+//        $user_phone = db('user')->field('user_phone')->select();
+        //dump($user_phone);die;
+//        $this->assign('accounts',$user_phone);
         $this->assign('userInfo', $this->_userinfo);
         $navi = Db('auth')->where('auth_pid',0)->order('auth_id', 'esc')->select();
         //dump( $navi);die;
         $this->assign('Navi',$navi);
+        $this->qrcode_logo();
+        $this->assign('imgurl',$this->Reimg);
         return $this->fetch();
     }
 
@@ -60,6 +81,88 @@ class Index extends Adminbase
             //手动登出时，清空forward
             //cookie("forward", NULL);
             $this->success('注销成功！');
+        }
+    }
+
+//二维码相关
+    public function re_qrcode()
+    {
+        if(@fopen( $this->filename, 'r' )){
+            unlink($this->filename);
+            $this->qrcode_logo();
+        }
+//        $result = array("code" => 1, "data" => array('msg' => 'success!', ));
+
+//        dump('asdfasdfaSD');die;
+//        return json($result);
+
+    }
+
+    public function qrcode()
+    {
+        $errorCorrectionLevel = 'L';
+        $matrixPointSize = 10;
+
+        $QR = $this->QRcode->png($this->url,$this->filename,$errorCorrectionLevel, $matrixPointSize);
+        //$QR = $this->filename;//已经生成的原始二维码图
+
+    }
+
+    public function qrcode_logo()
+    {
+
+        $errorCorrectionLevel = 'H';//容错级别
+        $matrixPointSize = 3;//生成图片大小
+        //生成二维码图片
+        if(!@fopen( $this->filename, 'r' )){
+            $this->QRcode->png($this->url,$this->filename,$errorCorrectionLevel, $matrixPointSize);
+            if(@fopen( $this->Reimg, 'r' )){
+                unlink($this->Reimg);
+            }
+        }
+        //$QR = 'qrcode.png';//已经生成的原始二维码图
+        if (FALSE !== $this->logo && !@fopen( $this->Reimg, 'r' )) {
+            /*$QR = imagecreatefromstring(file_get_contents($this->filename));
+            $logo = imagecreatefromstring(file_get_contents($logo));
+            $QR_width = imagesx($QR);//二维码图片宽度
+            $QR_height = imagesy($QR);//二维码图片高度
+            $logo_width = imagesx($logo);//logo图片宽度
+            $logo_height = imagesy($logo);//logo图片高度
+            $logo_qr_width = $QR_width / 5;
+            $scale = $logo_width/$logo_qr_width;
+            $logo_qr_height = $logo_height/$scale;
+            $from_width = ($QR_width - $logo_qr_width) / 2;*/
+
+            $dst_image = imagecreatefromstring(file_get_contents($this->logo));
+
+            $src_image = imagecreatefromstring(file_get_contents($this->filename));
+
+            $BG_width = imagesx($dst_image);//背景图宽度
+            $BG_height = imagesy($dst_image);//背景图高度
+
+            $src_width = imagesx($src_image);//抠图的宽度
+            $src_height = imagesy($src_image);//抠图的高度
+
+            $dst_w = 180;   //dst 区域宽度
+            //$scale = $QR_width/$dst_w;
+            $dst_h = 180;   //dst 区域高度
+            /*		    $dst_w = 150; 	//dst 区域宽度
+                        //$scale = $QR_width/$dst_w;
+                        $dst_h = 150; 	//dst 区域高度
+            */		    $dst_x = 1225;     //dst 区域x坐标
+            $dst_y = 380;   //dat 区域y坐标·
+            // $dst_x = 685; 	//dst 区域x坐标
+            // $dst_y = 220;	//dat 区域y坐标·
+            //dump($dst_x.'+'.$dst_y);die;
+            //重新组合图片并调整大小   将一幅图像中的一块正方形区域拷贝到另一个图像中，平滑地插入像素值，
+            //$dst_image：目标图象连接资源  $src_image：源图象连接资源。 $dst_x：目标 X 坐标点。 $dst_y：目标 Y 坐标点。 $src_x：源的 X 坐标点 $src_y：源的 Y 坐标点。$dst_w：目标宽度。 $dst_h：目标高度 $src_w：源图象的宽度 $src_h：源图象的高度。 坐标指的是左上角 在dst_image内根据坐标制作一个区域，将src_image内根据坐标抠出来的图放进去，缩放！
+            //imagecopyresampled ( resource $dst_image , resource $src_image , int $dst_x , int $dst_y , int $src_x , int $src_y , int $dst_w , int $dst_h , int $src_w , int $src_h ) : bool
+            imagecopyresampled($dst_image, $src_image, $dst_x, $dst_y, 0, 0, $dst_w, $dst_h, $src_width, $src_height);
+            Header("Content-type: image/png");
+            //echo "<style>img{ width:100%;height:100%;  }</style>";
+
+            ImagePng($dst_image,$this->Reimg);
+            exit();
         }
     }
 }
