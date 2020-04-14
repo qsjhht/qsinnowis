4baae2a8bd62aa3ef4499249c1a80655b43b97d9
ke_lin10 <ke_lin10@126.com>
2020-04-14 15:08:46 +0800

    应急管理主页

--- application/index/controller/Contingency.php
+++ application/index/controller/Contingency.php
@@ -16,14 +16,14 @@ class Contingency extends Adminbase
 {
     public function index()
     {
-//        //根据当前控制器名 ->auth_id  ->子类
-//        $auth_id = Db('auth')->where('auth_c',$this->request->controller())->where('auth_pid','0')->field('auth_id')->find();
-//        $sub = Db('auth')->where('auth_pid',$auth_id['auth_id'])->order('auth_id','esc')->select();
-//        $defaultUrl = ($sub['0']['auth_isLink']) ? $sub['0']['auth_url'] : $sub['0']['auth_c'].'/'.$sub['0']['auth_a'];
-//        $is_http = ($sub['0']['auth_isLink']) ? 1 : 0;
-//        $this->assign('Sub',$sub);
-//        $this->assign('defaultUrl',$defaultUrl);
-//        $this->assign('is_http',$is_http);
+        //根据当前控制器名 ->auth_id  ->子类
+        $auth_id = Db('auth')->where('auth_c',$this->request->controller())->where('auth_pid','0')->field('auth_id')->find();
+        $sub = Db('auth')->where('auth_pid',$auth_id['auth_id'])->order('auth_id','esc')->select();
+        $defaultUrl = ($sub['0']['auth_isLink']) ? $sub['0']['auth_url'] : $sub['0']['auth_c'].'/'.$sub['0']['auth_a'];
+        $is_http = ($sub['0']['auth_isLink']) ? 1 : 0;
+        $this->assign('Sub',$sub);
+        $this->assign('defaultUrl',$defaultUrl);
+        $this->assign('is_http',$is_http);
         return $this->fetch();
     }
 
